505e305e073e0e774e1c00b4e8f7f6b03cbf68a0
373376253 <373376253@users.noreply.github.com>
2019-04-16 16:29:10 +0800

    3

--- Console.App/Console.App/Program.cs
+++ Console.App/Console.App/Program.cs
@@ -8,6 +8,8 @@ namespace Console.App {
     class Program {
         static void Main(string[] args) {
             System.Console.WriteLine("");
+            System.Console.WriteLine("");
+            System.Console.WriteLine("");
         }
     }
 }
